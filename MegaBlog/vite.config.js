7db6f14297dff3b302833026be6d5bcb6e2f9b87
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-02-01 21:30:06 +0600

    final fix

--- MegaBlog/vite.config.js
+++ MegaBlog/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  // এটি নিশ্চিত করবে যে সব ফাইল রুট পাথ থেকে লোড হচ্ছে
+  base: '/', 
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
